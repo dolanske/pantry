e82786969a1934c463b2ce4bac3ea23a284ef90f
dolanske <dolanovsky@gmail.com>
2024-01-29 23:33:12 +0200

    Expose `navigate` from crumbs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { onNavigation, onRouteError, onRouteResolve } from '@dolanske/crumbs'
+import { navigate, onNavigation, onRouteError, onRouteResolve } from '@dolanske/crumbs'
 import type { Component } from '@dolanske/cascade'
 import { El, getInstance } from '@dolanske/cascade'
 import { RouterLink } from './link'
@@ -8,10 +8,11 @@ import { createApp } from './pantry'
 export {
   RouterLink,
   createApp,
-  // Router methods
+  // Crumbs
   onRouteError,
   onRouteResolve,
   onNavigation,
+  navigate,
   // Cascade
   El,
   getInstance,
