956eb58cb863e842224cf76ee5743b0ac7b0d0eb
dolanske <dolanovsky@gmail.com>
2024-01-29 23:33:22 +0200

    Update pantry.js

--- dist/pantry.js
+++ dist/pantry.js
@@ -1,9 +1,9 @@
-import { navigate as v, defineRouter as b, onRouteResolve as h, onRouteError as k } from "@dolanske/crumbs";
-import { onNavigation as D, onRouteError as F, onRouteResolve as I } from "@dolanske/crumbs";
-import { El as E, Component as g } from "@dolanske/cascade";
-import { El as N, getInstance as O } from "@dolanske/cascade";
+import { navigate as v, defineRouter as b, onRouteResolve as g, onRouteError as h } from "@dolanske/crumbs";
+import { navigate as D, onNavigation as F, onRouteError as I, onRouteResolve as L } from "@dolanske/crumbs";
+import { El as k, Component as E } from "@dolanske/cascade";
+import { El as O, getInstance as V } from "@dolanske/cascade";
 function y(s, r) {
-  return E.a().setup((n) => {
+  return k.a().setup((n) => {
     n.attr("href", r), n.nest(s), n.click((a) => {
       a.preventDefault(), v(r);
     });
@@ -15,7 +15,7 @@ function A(s) {
   const r = {}, n = {}, a = {};
   for (const [e, t] of Object.entries(s)) {
     const o = "<div route-boundary>";
-    if (t instanceof g)
+    if (t instanceof E)
       r[e] = t, a[e] = {
         html: o
       };
@@ -33,10 +33,10 @@ function A(s) {
   let i = p, c = p;
   return {
     run: (e) => {
-      l.run(e), i = h((t) => {
+      l.run(e), i = g((t) => {
         const o = r[t.path];
         o.props({ data: t.data }), o && o.mount("[router-boundary]");
-      }), c = k((t, o) => {
+      }), c = h((t, o) => {
         if (!t) {
           console.warn("Attempted to navigate to a route that does not exist"), console.error(o);
           return;
@@ -51,11 +51,12 @@ function A(s) {
   };
 }
 export {
-  N as El,
+  O as El,
   y as RouterLink,
   A as createApp,
-  O as getInstance,
-  D as onNavigation,
-  F as onRouteError,
-  I as onRouteResolve
+  V as getInstance,
+  D as navigate,
+  F as onNavigation,
+  I as onRouteError,
+  L as onRouteResolve
 };
